948740b6ad421288ecb2440addb470fa9ae50aa1
GateX5 <128730728+GateX5@users.noreply.github.com>
2023-03-30 17:28:43 +0200

    Update parametrat.py

--- parametrat.py
+++ parametrat.py
@@ -1,3 +1,30 @@
+
+class Game(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,speed,size_x,size_y):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image),(size_x,size_y))
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(Game):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 600:
+            self.rect.y += self.speed 
+    def update1(self):
+        keys = key.get_pressed()
+        if keys[K_i] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_k] and self.rect.y < 600:
+            self.rect.y += self.speed 
+
 ww = 800
 wh = 600
 win = display.set_mode((ww,wh))
